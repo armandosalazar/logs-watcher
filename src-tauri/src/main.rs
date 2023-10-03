44523cd00e0836e9be6f5d08e0eff29fe8ffa67b
Armando Salazar <armandosalazar351@gmail.com>
2023-10-02 23:16:12 -0600

    feat: add file explorer dialog and read file

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn print(content: &str) {
-    println!("{}", content);
+fn log(content: &str) {
+    let now = chrono::Local::now();
+    println!("\x1b[35m[ {:#?} ]\x1b[0m {}", now, content);
 }
 
 #[tauri::command]
@@ -12,24 +14,9 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-#[tauri::command]
-fn open() -> String {
-    // run a command
-    // std::process::Command::new("cmd")
-    //     .args(&["/C", "start", "https://tauri.studio"])
-    //     .spawn()
-    //     .expect("failed to execute process");
-    std::process::Command::new("bash")
-        .args(&["-c", "open https://tauri.studio"])
-        .spawn()
-        .expect("failed to execute process");
-
-    format!("Hello, {}! You've been greeted from Rust!", "open")
-}
-
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![print, greet, open])
+        .invoke_handler(tauri::generate_handler![log, greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,47 @@
-import { useState } from "react";
+import {useState} from "react";
 import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
-import { dialog } from "@tauri-apps/api";
+import {invoke} from "@tauri-apps/api/tauri";
+import {dialog, fs} from "@tauri-apps/api";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-  const [path, setPath] = useState("");
+    const [path, setPath] = useState("");
+    const [content, setContent] = useState("");
 
-  async function print(content) {
-    await invoke("print", { content });
-  }
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
-  async function handleOpen() {
-    try {
-      const result = await dialog.open({
-        multiple: false,
-        directory: false,
-        filter: "png",
-      });
-      setPath(result);
-      print(result);
-    } catch (error) {
-      console.log(error);
+    async function log(content) {
+        await invoke("log", {content});
     }
-  }
 
-  return (
-    <>
-    <pre>{path}</pre>
+    async function handleOpen() {
+        try {
+            const result = await dialog.open({
+                multiple: false,
+                directory: false,
+                filter: "png",
+            });
 
-      <button type="button" onClick={handleOpen}>Open file</button>
+            if (typeof result === "string") {
+                setPath(result);
+                try {
+                    const fileContent = await fs.readTextFile(result);
+                    setContent(fileContent);
+                } catch (error) {
+                    await log(error);
+                }
+            }
 
-    </>
-  );
+            await log(result);
+        } catch (error) {
+            await log(error);
+        }
+    }
+
+    return (
+        <>
+            <pre>{path}</pre>
+            <button type="button" onClick={handleOpen}>Open file</button>
+        </>
+    );
 }
 
 export default App;
