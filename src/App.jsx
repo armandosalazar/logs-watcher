55e0698145b92a62d518edcb86b2f6b77d0f2114
Armando Salazar <armandosalazar351@gmail.com>
2023-10-03 00:55:03 -0600

    feat: add icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,61 @@
-import {useState} from "react";
+import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import {invoke} from "@tauri-apps/api/tauri";
-import {dialog, fs} from "@tauri-apps/api";
+import { invoke } from "@tauri-apps/api/tauri";
+import { dialog, fs } from "@tauri-apps/api";
 import "./App.css";
-import {Button} from "@mui/material";
+import { Button } from "@mui/material";
+import FileUploadIcon from '@mui/icons-material/FileUpload';
 
 function App() {
-    const [path, setPath] = useState("");
-    const [content, setContent] = useState("");
-    const [length, setLength] = useState(0);
-    const interval = setInterval(async () => {
-        let fileContent = await fs.readTextFile(path);
-        setContent(fileContent);
-    }, 1000);
+  const [path, setPath] = useState("");
+  const [content, setContent] = useState("");
+  let length = 0;
 
-    async function log(content) {
-        await invoke("log", {content});
+  const interval = setInterval(async () => {
+    const fileContent = await fs.readTextFile(path);
+    if (length == 0) {
+      length = fileContent.length;
+      setContent(fileContent);
     }
 
-    async function handleOpen() {
-        try {
-            const result = await dialog.open({
-                multiple: false,
-                directory: false,
-                filter: "png",
-            });
-
-            if (typeof result === "string") {
-                setPath(result);
-            }
-            await log(result);
-
-        } catch (error) {
-            await log(error);
-        }
+    if (length != fileContent.length) {
+      length = fileContent.length;
+      setContent(fileContent);
     }
 
-    return (
-        <>
-            <pre>{path}</pre>
-            <button type="button" onClick={handleOpen}>Open file</button>
-            <Button variant="contained" onClick={handleOpen}>Open file</Button>
-            <pre>{content}</pre>
-        </>
-    );
+  }, 1000);
+
+  async function log(content) {
+    await invoke("log", { content });
+  }
+
+  async function handleOpen() {
+    try {
+      const result = await dialog.open({
+        multiple: false,
+        directory: false,
+        filter: "png",
+      });
+
+      if (typeof result === "string") {
+        setPath(result);
+      }
+      await log(result);
+
+    } catch (error) {
+      await log(error);
+    }
+  }
+
+  return (
+    <>
+      <pre>{path}</pre>
+      <Button style={{
+        margin: "auto",
+      }} variant="outlined" startIcon={<FileUploadIcon/>} onClick={handleOpen}>open file</Button>
+      <pre>{content}</pre>
+    </>
+  );
 }
 
 export default App;
